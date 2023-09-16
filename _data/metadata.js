beb9c29e7d828c1fa363fe6fd58f94f26b17411a
Baptiste <baptiste@roullin.net>
2023-09-16 20:00:14 +0200

    removed unused

--- _data/metadata.js
+++
@@ -1,9 +0,0 @@
-let data = {
-	username: "eleven_ty", // No leading @ here
-	homeLabel: "11ty.dev",
-	homeUrl: "https://www.11ty.dev/",
-};
-
-data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
-
-module.exports = data;
